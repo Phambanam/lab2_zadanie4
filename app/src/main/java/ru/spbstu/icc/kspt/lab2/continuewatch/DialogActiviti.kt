a5433b905870c531b2454770bbed83743d6d91a8
phambanam <phambanam1999@gmail.com>
2022-01-14 09:13:40 +0300

    lab2

---
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/DialogActiviti.kt
@@ -0,0 +1,12 @@
+package ru.spbstu.icc.kspt.lab2.continuewatch
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DialogActiviti : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.dialog)
+    }
+}
--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,10 +1,15 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+const val  TAG: String = "my_log"
 const val SECONDS_ELAPSED = "seconds elapsed"
 class MainActivity : AppCompatActivity() {
     var secondsElapsed: Int = 0
@@ -16,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         while (true) {
             if (count) {
                 textSecondsElapsed.post {
-                    textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
+                    textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed++
 
                 }
                 Thread.sleep(1000)
@@ -27,20 +32,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"OnCreate")
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
         sharedPreferences = getSharedPreferences(SECONDS_ELAPSED, Context.MODE_PRIVATE)
         backgroundThread.start()
-    }
+        val btnActivity = findViewById<Button>(R.id.btn_activity)
+        btnActivity.setOnClickListener{
+           startActivity(Intent(this,DialogActiviti::class.java))
 
-    override fun onResume() {
-        super.onResume()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart")
         count = true
         secondsElapsed = sharedPreferences.getInt(SECONDS_ELAPSED, 0)
 
     }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"onRestart")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"Destroy")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"OnResume")
+
+
+    }
+
     override fun onStop() {
         super.onStop()
+        Log.d(TAG,"OnStop")
         count = false
         sharedPreferences.edit().run {
             putInt(SECONDS_ELAPSED, secondsElapsed)
